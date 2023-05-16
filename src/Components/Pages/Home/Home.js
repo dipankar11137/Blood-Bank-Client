c9d32f5f0366403c455d83ffbe3b771b1ae10133
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-17 00:46:06 +0600

    add contace

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import Bloods from "./Blood/Bloods";
+import Contact from "./Contact/Contact";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Bloods />
+      <Contact />
     </div>
   );
 };
