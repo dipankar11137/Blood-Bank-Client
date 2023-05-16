01a71cf2cea2ec83d485900e76489d0fa18fbe24
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-17 00:55:30 +0600

    edit blood

--- src/Components/Pages/Home/Blood/Blood.js
+++ src/Components/Pages/Home/Blood/Blood.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blood = () => {
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl hover:cursor-pointer hover:shadow-2xl hover:bg-lime-100">
       <figure>
         <img
           src="https://www.nzblood.co.nz/assets/Column/Blood-Type-Blood-Drops-A+__ScaleMaxWidthWzcwMF0.png"
@@ -10,11 +10,13 @@ const Blood = () => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Blood Group : A+</h2>
-        <p>Quantity : 10</p>
-        <p>Price : 10</p>
+        <h2 className="card-title text-3xl font-bold">Blood Group : A+</h2>
+        <p className="text-xl font-semibold">Quantity : 10</p>
+        <p className="text-xl font-semibold">Price : $10</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary text-white text-xl">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
--- src/Components/Pages/Home/Blood/Bloods.js
+++ src/Components/Pages/Home/Blood/Bloods.js
@@ -11,7 +11,10 @@ const Bloods = () => {
       .then((data) => setBloods(data));
   }, []);
   return (
-    <div className="mt-20 mx-20">
+    <div className="mt-20 mx-20 bg-slate-50 rounded-xl">
+      <h1 className="text-center text-5xl font-bold mb-5 pt-5">
+        Our Available Bloods
+      </h1>
       <div className="grid grid-cols-4 gap-x-4 gap-y-8">
         {bloods.map((blood) => (
           <Blood key={blood._id} blood={blood}></Blood>
