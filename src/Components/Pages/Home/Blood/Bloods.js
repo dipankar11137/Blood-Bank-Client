7ef202dee09e2da64492c4ce28b7d91a81ae3b4e
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-10 00:28:23 +0600

    blood connect backend

--- src/Components/Pages/Home/Blood/Bloods.js
+++ src/Components/Pages/Home/Blood/Bloods.js
@@ -1,22 +1,20 @@
-import React from "react";
-import Blood from "./Blood";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from 'react';
+import Blood from './Blood';
 
 const Bloods = () => {
   const [bloods, setBloods] = useState([]);
   useEffect(() => {
-    fetch("products.json")
-      .then((res) => res.json())
-      .then((data) => setBloods(data));
-  }, []);
+    fetch('http://localhost:5000/bloods')
+      .then(res => res.json())
+      .then(data => setBloods(data));
+  }, [bloods]);
   return (
     <div className="mt-20 mx-20 bg-slate-50 rounded-xl">
       <h1 className="ml-5 text-7xl font-bold mb-5 pt-5">
         Our Available Bloods
       </h1>
-      <div className="grid grid-cols-4 gap-x-4 gap-y-8">
-        {bloods.map((blood) => (
+      <div className="grid grid-cols-4 gap-x-4 gap-y-8 p-3">
+        {bloods.map(blood => (
           <Blood key={blood._id} blood={blood}></Blood>
         ))}
       </div>
