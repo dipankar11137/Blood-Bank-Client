ff35db873e76a2c8bf513aed03df3dd7a7fb6be2
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-14 00:52:13 +0600

    add manage donate blood

--- src/Components/Pages/Dashboard/Manage Donate Blood/ManageDonateBlood.js
+++ src/Components/Pages/Dashboard/Manage Donate Blood/ManageDonateBlood.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
-const ManageDonateBlood = () => {
+const ManageDonateBlood = ({ mBlood, index, handleDelete }) => {
   return (
-    <div>
-      <h1>Donate blood</h1>
-    </div>
+    <tr>
+      <th>{index}</th>
+      <td>{mBlood?.name}</td>
+      <td>{mBlood?.studentId}</td>
+      <td>{mBlood?.bloodGroup}</td>
+      <td>{mBlood?.age}</td>
+      <td>{mBlood?.weight}</td>
+      <td>{mBlood?.date}</td>
+      <td>{mBlood?.time}</td>
+      <td>{mBlood?.phone}</td>
+      <td>{mBlood?.address}</td>
+
+      <td>
+        {' '}
+        <button
+          // onClick={() => handleAccept(mBlood?._id)}
+          className="btn btn-secondary  btn-sm text-white pt-1 pic-style"
+        >
+          Accept
+        </button>
+      </td>
+      <td>
+        {' '}
+        <button
+          onClick={() => handleDelete(mBlood?._id)}
+          className="btn btn-primary  btn-sm text-white pic-style"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
   );
 };
 
--- src/Components/Pages/Dashboard/Manage Donate Blood/ManageDonateBloods.js
+++ src/Components/Pages/Dashboard/Manage Donate Blood/ManageDonateBloods.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ManageDonateBlood from './ManageDonateBlood';
 
 const ManageDonateBloods = () => {
+  const [mBloods, setMBloods] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/donateBlood`)
+      .then(res => res.json())
+      .then(data => setMBloods(data));
+  }, [mBloods]);
+  const handleDelete = id => {};
   return (
     <div>
-      <ManageDonateBlood />
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr className="text-center">
+              <th className="text-xl">Index</th>
+              <th className="text-xl">Name</th>
+              <th className="text-xl">Student Id</th>
+              <th className="text-xl">Blood Group</th>
+              <th className="text-xl">Age</th>
+              <th className="text-xl">Weight</th>
+              <th className="text-xl">Date</th>
+              <th className="text-xl">Time</th>
+              <th className="text-xl">Phone</th>
+              <th className="text-xl">Address</th>
+              <th className="text-xl">Accept</th>
+              <th className="text-xl">Delete</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {mBloods.map((mBlood, index) => (
+              <ManageDonateBlood
+                key={mBlood._id}
+                mBlood={mBlood}
+                index={index + 1}
+                handleDelete={handleDelete}
+              ></ManageDonateBlood>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
